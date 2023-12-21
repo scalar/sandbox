4a53b3e5dc8f62852399ef5d02e4fa9b9d3fd1f7
Hans Pagel <mail@hanspagel.com>
2023-12-21 23:22:02 +0100

    init :)

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+import { ApiReference } from '@scalar/api-reference';
+import MonacoEditor from './components/MonacoEditor.vue'
+</script>
+
+<template>
+  <div class="app">
+    <header class="header">
+      <div class="logo">
+        Play
+      </div>
+      <div class="actions">
+        <button type="button">
+          Share
+        </button>
+      </div>
+    </header>
+    <div class="layout">
+      <div class="left">
+        <MonacoEditor />
+      </div>
+      <div class="right">
+        <ApiReference />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.app {
+  font-family: var(--default-theme-font)
+}
+.header {
+  height: 50px;
+  border-bottom: 1px solid var(--default-theme-border-color);
+  color: var(--default-theme-color-1);
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 12px;
+}
+
+.logo {
+  font-size: var(--default-theme-small);
+  font-weight: bold;
+}
+.layout {
+  display: flex;
+  flex-direction: row;
+  height: calc(100vh - 50px);
+}
+.left {
+  width: 50%;
+  height: 100%;
+  border-right: 1px solid var(--default-theme-border-color);
+}
+.right {
+  width: 50%;
+  height: 100%;
+}
+</style>
+
---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    nodePolyfills({
+      // To exclude specific polyfills, add them to this list.
+      exclude: [
+        'fs', // Excludes the polyfill for `fs` and `node:fs`.
+      ],
+      // Whether to polyfill specific globals.
+      globals: {
+        Buffer: true, // can also be 'build', 'dev', or false
+        global: true,
+        process: true,
+      },
+      // Whether to polyfill `node:` protocol imports.
+      protocolImports: true,
+    }),
+  ],
+})
