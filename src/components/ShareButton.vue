b23299350a9e2621e7152afa298df5a5af9135ad
Hans Pagel <mail@hanspagel.com>
2024-01-17 01:29:10 +0100

    style: basic share button styles

--- src/components/ShareButton.vue
+++ src/components/ShareButton.vue
@@ -9,7 +9,19 @@ defineEmits<{
 </script>
 
 <template>
-  <button type="button" @click="$emit('share')" :disabled="disabled">
+  <button type="button" @click="$emit('share')" :disabled="disabled" class="button">
     Share
   </button>
 </template>
+
+<style scoped>
+.button {
+  background: var(--default-theme-background-3);
+  font-weight: normal;
+  border: none;
+  font-family: var(--default-theme-font);
+  font-size: var(--default-theme-small);
+  padding: 6px 12px;
+  border-radius: var(--default-theme-radius-lg);
+}
+</style>
