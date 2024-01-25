5b38f3cb3595c88ba73e53257cf91eb4617e9a98
Brynn Hawker <brynn@hawker.me>
2024-01-25 14:59:27 -0800

    feat: clear up header flex

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,8 @@ import ShareButton from './components/ShareButton.vue'
 import { Toaster, toast } from 'vue-sonner'
 import { useDark } from '@vueuse/core'
 import ModeToggleButton from './components/ModeToggleButton.vue';
+import GitHubLogo from './components/GithubLogo.vue';
+import ScalarLogo from './components/ScalarLogo.vue';
 
 const isDark = useDark()
 
@@ -109,7 +111,7 @@ function handleDrop(text: string) {
 }
 
 onMounted(() => {
-  window.onbeforeunload = function() {
+  window.onbeforeunload = function () {
     if (contentChanged.value) {
       return false
     }
@@ -127,7 +129,7 @@ onUnmounted(() => {
     <div class="app">
       <header class="header">
         <div class="logo">
-          <svg width="25" height="25" viewBox="0 0 593 593" xmlns="http://www.w3.org/2000/svg"><path d="M347 0c6 0 12 5 12 12v134l94-95c5-5 13-5 17 0l72 72c4 4 5 12 0 16v1l-95 94h134c7 0 12 5 12 12v101c0 7-5 12-12 12H447l95 94c4 5 5 13 0 17l-72 72c-4 4-12 5-16 0h-1l-94-95v134c0 7-5 12-12 12H246c-7 0-12-5-12-12v-70c0-22 9-43 24-59l130-130c14-14 14-37 0-51L259 142a84 84 0 0 1-25-59V12c0-7 5-12 12-12h101ZM138 52h1l219 219c14 14 14 37 0 51L139 542c-4 5-12 5-17 0l-71-70c-4-5-5-12 0-17l95-96H12c-7 0-12-5-12-12V246c0-7 5-12 12-12h134l-95-94c-4-5-4-12 0-17l71-71c4-5 12-5 16 0Z" fill="currentColor" fill-rule="evenodd"></path></svg>
+          <ScalarLogo />
           <div class="logo-text">
             Sandbox
           </div>
@@ -138,12 +140,7 @@ onUnmounted(() => {
         <div class="actions">
           <DarkModeToggle />
           <a href="https://github.com/scalar/scalar">
-            <!-- make the SVG 20px high-->
-            <svg width="20" height="20" viewBox="0 0 98 96" xmlns="http://www.w3.org/2000/svg" class="github-logo">
-                <path fill-rule="evenodd" clip-rule="evenodd"
-                    d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z"
-                    fill="currentColor" />
-            </svg>
+            <GitHubLogo />
           </a>
           <ShareButton @click="share" :loading="loading" />
         </div>
@@ -164,23 +161,38 @@ onUnmounted(() => {
 .app {
   font-family: var(--default-theme-font)
 }
+
 .header {
   height: 50px;
   border-bottom: 1px solid var(--default-theme-border-color);
   color: var(--default-theme-color-1);
   display: flex;
   align-items: center;
-  justify-content: space-between;
   padding: 12px;
 }
 
-.actions {
+.header>* {
   display: flex;
-  gap: 20px;
+  justify-content: center;
   align-items: center;
+  flex: 1;
 }
 
-.mode a, .mode button, .actions a {
+.header> :first-child {
+  justify-content: start;
+}
+
+.header> :last-child {
+  justify-content: end;
+}
+
+.actions {
+  gap: 12px;
+}
+
+.mode a,
+.mode button,
+.actions a {
   text-decoration: none;
   color: var(--default-theme-color-1);
   font-size: var(--default-theme-small);
@@ -200,20 +212,21 @@ onUnmounted(() => {
 .logo {
   font-size: var(--default-theme-small);
   font-weight: var(--default-theme-bold);
-  display: flex;
-  align-items: center;
   gap: 8px;
 }
+
 .layout {
   display: flex;
   flex-direction: row;
   height: calc(100vh - 50px);
 }
+
 .left {
   width: 50%;
   height: 100%;
   border-right: 1px solid var(--default-theme-border-color);
 }
+
 .right {
   width: 100%;
   height: 100%;
--- src/components/DarkModeToggle.vue
+++ src/components/DarkModeToggle.vue
@@ -6,23 +6,47 @@ const toggleDark = useToggle(isDark)
 </script>
 
 <template>
-  <button type="button" @click="() => toggleDark()" class="button">
+  <button type="button" @click="() => toggleDark()" class="darkmode-toggle">
     <template v-if="isDark">
-      Light Mode
+      <svg width="20px" height="20px" viewBox="0 0 10 10" xmlns="http://www.w3.org/2000/svg">
+        <path d="M3.500 5.000 A1.500 1.500 0 1 0 6.500 5.000 A1.500 1.500 0 1 0 3.500 5.000 Z" fill="currentColor"
+          stroke="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="0"></path>
+        <path d="M5,2.25a.625.625,0,0,1-.625-.625v-1a.625.625,0,0,1,1.25,0v1A.625.625,0,0,1,5,2.25Z" fill="currentColor"
+          stroke="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="0"></path>
+        <path
+          d="M6.945,3.055a.623.623,0,0,1,0-.883l.707-.708a.625.625,0,0,1,.884.884l-.708.707A.623.623,0,0,1,6.945,3.055Z"
+          fill="currentColor" stroke="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="0"></path>
+        <path d="M7.75,5a.625.625,0,0,1,.625-.625h1a.625.625,0,0,1,0,1.25h-1A.625.625,0,0,1,7.75,5Z" fill="currentColor"
+          stroke="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="0"></path>
+        <path
+          d="M6.945,6.945a.623.623,0,0,1,.883,0l.708.707a.625.625,0,0,1-.884.884l-.707-.708A.623.623,0,0,1,6.945,6.945Z"
+          fill="currentColor" stroke="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="0"></path>
+        <path d="M5,7.75a.625.625,0,0,1,.625.625v1a.625.625,0,0,1-1.25,0v-1A.625.625,0,0,1,5,7.75Z" fill="currentColor"
+          stroke="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="0"></path>
+        <path
+          d="M3.055,6.945a.623.623,0,0,1,0,.883l-.707.708a.625.625,0,0,1-.884-.884l.708-.707A.623.623,0,0,1,3.055,6.945Z"
+          fill="currentColor" stroke="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="0"></path>
+        <path d="M2.25,5a.625.625,0,0,1-.625.625h-1a.625.625,0,0,1,0-1.25h1A.625.625,0,0,1,2.25,5Z" fill="currentColor"
+          stroke="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="0"></path>
+        <path
+          d="M3.055,3.055a.623.623,0,0,1-.883,0l-.708-.707a.625.625,0,0,1,.884-.884l.707.708A.623.623,0,0,1,3.055,3.055Z"
+          fill="currentColor" stroke="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="0"></path>
+      </svg>
     </template>
     <template v-else>
-      Dark Mode
+      <svg width="20px" height="20px" viewBox="0 0 10 10" xmlns="http://www.w3.org/2000/svg">
+        <path
+          d="M9.557,7.243A.456.456,0,0,0,9.1,6.991C9.023,7,8.951,7,8.877,7A4,4,0,0,1,5.546.781.5.5,0,0,0,5.564.256.476.476,0,0,0,5.1,0,5,5,0,1,0,9.539,7.767.5.5,0,0,0,9.557,7.243Z"
+          fill="currentColor" stroke="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="0"></path>
+      </svg>
     </template>
   </button>
 </template>
 
 <style scoped>
-.button {
-  background: none;
-  padding: 0;
-  color: var(--default-theme-color-3);
-  font-weight: normal;
-  border: none;
+.darkmode-toggle {
+  color: var(--default-theme-color-1);
+  cursor: pointer;
   font-family: var(--default-theme-font);
   font-size: var(--default-theme-small);
   border-radius: var(--default-theme-radius-lg);
